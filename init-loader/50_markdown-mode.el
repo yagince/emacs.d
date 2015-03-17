b9b0c2746c3909d8d41828b1bcfe578623a920cc
Yagi <me-me-@fout.jp>
2015-03-17 22:49:18 +0900

    fixed markdown mode setting

--- init-loader/50_markdown-mode.el
+++ init-loader/50_markdown-mode.el
@@ -1,6 +1,9 @@
 ;; markdown-mode
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+
+(add-hook 'markdown-mode-hook '(lambda ()
+                                 (electric-indent-local-mode -1)))
