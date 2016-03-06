cbae26d4079c35d600fac0c704a454c5515054da
yagince <straitwalk@gmail.com>
2016-03-06 10:53:58 +0900

    日本語設定

--- init-loader/00_base.el
+++ init-loader/00_base.el
@@ -76,3 +76,17 @@
 
 ;; "yes or no" の選択を "y or n" にする
 (fset 'yes-or-no-p 'y-or-n-p)
+
+;; 英語
+ (set-face-attribute 'default nil
+             :family "Menlo" ;; font
+             )    ;; font size
+
+;; 日本語
+(set-fontset-font
+ nil 'japanese-jisx0208
+  (font-spec :family "Hiragino Kaku Gothic ProN")) ;; font
+
+;; 半角と全角の比を1:2にしたければ
+(setq face-font-rescale-alist
+      '((".*Hiragino_Kaku_Gothic_ProN.*" . 1.2)));; Mac用フォント設定
