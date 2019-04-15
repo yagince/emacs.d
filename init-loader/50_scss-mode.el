23781fbe73ae89fad768735a5e362b96855bb287
yagince <straitwalk@gmail.com>
2019-04-15 23:42:03 +0900

    scss-mode to use use-package defer

--- init-loader/50_scss-mode.el
+++ init-loader/50_scss-mode.el
@@ -1,14 +1,14 @@
-(require 'scss-mode)
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-
-;; インデント幅を2にする
-;; コンパイルは compass watchで行うので自動コンパイルをオフ
-(defun scss-custom ()
-  "scss-mode-hook"
-  (and
-   (set (make-local-variable 'css-indent-offset) 2)
-   (set (make-local-variable 'scss-compile-at-save) nil)
-   )
+(use-package scss-mode
+  :mode (("\\.scss$" . scss-mode))
+  :init
+  (defun scss-custom ()
+    "scss-mode-hook"
+    (and
+     (set (make-local-variable 'css-indent-offset) 2) ;; インデント幅を2にする
+     (set (make-local-variable 'scss-compile-at-save) nil) ;; コンパイルは compass watchで行うので自動コンパイルをオフ
+     )
+    )
+  :config
+  (add-hook 'scss-mode-hook
+            '(lambda() (scss-custom)))
   )
-(add-hook 'scss-mode-hook
-  '(lambda() (scss-custom)))
