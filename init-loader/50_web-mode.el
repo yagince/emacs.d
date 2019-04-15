82d2f5beba4732dd5c59716035804cf098b98a1c
yagince <straitwalk@gmail.com>
2019-04-15 23:47:03 +0900

    web-mode to use use-package defer

--- init-loader/50_web-mode.el
+++ init-loader/50_web-mode.el
@@ -1,23 +1,18 @@
-;; web-mode
-(require 'web-mode)
-;; 拡張子の設定
-(add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
-;; (add-to-list 'auto-mode-alist '("\\.vue?$"      . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js?$"       . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css?$"      . web-mode))
-;; インデント関係
-(defun web-mode-setting-hook ()
-  "Hooks for Web mode."
+(use-package web-mode
+  :mode (
+         ("\\.phtml$"     . web-mode)
+         ("\\.tpl\\.php$" . web-mode)
+         ("\\.jsp$"       . web-mode)
+         ("\\.as[cp]x$"   . web-mode)
+         ("\\.erb$"       . web-mode)
+         ("\\.html?$"     . web-mode)
+         ("\\.js?$"       . web-mode)
+         ("\\.css?$"      . web-mode)
+         ("\\.jsx$"       . web-mode)
+         )
+  :config
   (setq web-mode-markup-indent-offset  2)
   (setq web-mode-css-indent-offset     2)
   (setq web-mode-code-indent-offset    2)
   (setq indent-tabs-mode               nil)
   )
-
-(add-hook 'web-mode-hook 'web-mode-setting-hook)
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
