ea23acf91dd129790a2449b8a3984009d32187c4
yagince <straitwalk@gmail.com>
2019-04-15 23:04:31 +0900

    markdown-mode to use use-package defer

--- init-loader/50_markdown-mode.el
+++ init-loader/50_markdown-mode.el
@@ -1,10 +1,20 @@
-;; markdown-mode
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-;; (add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-;; (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-;; (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+;; ;; markdown-mode
+;; (autoload 'markdown-mode "markdown-mode"
+;;    "Major mode for editing Markdown files" t)
+;; ;; (add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
+;; (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+;; ;; (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+;; ;; (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
-(add-hook 'markdown-mode-hook '(lambda ()
-                                 (electric-indent-local-mode -1)))
+;; (add-hook 'markdown-mode-hook '(lambda ()
+;;                                  (electric-indent-local-mode -1)))
+
+(use-package markdown-mode
+  :mode (
+         ("\\.markdown\\'" . markdown-mode)
+         ("\\.md\\'" . markdown-mode)
+         )
+  :config
+  (add-hook 'markdown-mode-hook '(lambda ()
+                                   (electric-indent-local-mode -1)))
+  )
