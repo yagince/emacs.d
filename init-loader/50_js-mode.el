d36f8cebdf2e1958799b3c18b5b6d2e33826522d
yagince <straitwalk@gmail.com>
2019-04-15 22:55:09 +0900

    js2-mode to use use-package defer

--- init-loader/50_js-mode.el
+++ init-loader/50_js-mode.el
@@ -1,8 +1,11 @@
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-hook 'js2-mode-hook
-          '(lambda ()
-             (setq js2-basic-offset 2
-                   tab-width        2
-                   indent-tabs-mode nil
-                   js2-cleanup-whitespace nil)))
+(use-package js2-mode
+  :mode (("\\.js$" . js2-mode))
+  :config
+  (add-hook 'js2-mode-hook
+            '(lambda ()
+               (setq js2-basic-offset 2
+                     tab-width        2
+                     indent-tabs-mode nil
+                     js2-cleanup-whitespace nil))
+            )
+  )
