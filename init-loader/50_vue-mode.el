d7a715d8da181e513ecc114184ad942cdbe2ab7d
yagince <straitwalk@gmail.com>
2019-04-15 23:44:18 +0900

    vue-mode to use use-package defer

--- init-loader/50_vue-mode.el
+++ init-loader/50_vue-mode.el
@@ -1,15 +1,12 @@
-(require 'flycheck)
-(add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
-(eval-after-load 'vue-mode
-  '(add-hook 'vue-mode-hook #'add-node-modules-path))
-(add-hook 'vue-mode-hook 'yas-minor-mode)
-(flycheck-add-mode 'javascript-eslint 'vue-mode)
-(flycheck-add-mode 'javascript-eslint 'vue-html-mode)
-(flycheck-add-mode 'javascript-eslint 'css-mode)
-(add-hook 'vue-mode-hook 'flycheck-mode)
-
-(add-hook 'mmm-mode-hook
-          (lambda ()
-            (set-face-background 'mmm-default-submode-face nil)))
-(setq indent-tabs-mode nil
-      js-indent-level 2)
+(use-package vue-mode
+  :mode (("\\.vue\\'" . vue-mode))
+  :config
+  (setq indent-tabs-mode nil
+        js-indent-level 2)
+  (add-hook 'vue-mode-hook #'add-node-modules-path)
+  (add-hook 'vue-mode-hook 'yas-minor-mode)
+  (add-hook 'vue-mode-hook 'flycheck-mode)
+  (flycheck-add-mode 'javascript-eslint 'vue-mode)
+  (flycheck-add-mode 'javascript-eslint 'vue-html-mode)
+  (flycheck-add-mode 'javascript-eslint 'css-mode)
+  )
