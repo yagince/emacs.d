4f1e8fa898dfce5710c19a03ad6db2bbd9a48510
yagince <straitwalk@gmail.com>
2019-04-15 22:49:59 +0900

    highlight-symbol to use use-package defer

--- init-loader/50_highligh_symbol.el
+++ init-loader/50_highligh_symbol.el
@@ -1,4 +1,6 @@
-(require 'highlight-symbol)
-(add-hook 'prog-mode-hook 'highlight-symbol-mode)
-(add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)
-
+(use-package highlight-symbol
+  :defer t
+  :init
+  (add-hook 'prog-mode-hook 'highlight-symbol-mode)
+  (add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)
+  )
