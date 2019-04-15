0686dd5ab4bd624b20fd6c8b03accf6ca4fa9d01
yagince <straitwalk@gmail.com>
2019-04-15 23:02:10 +0900

    magit to use use-package defer

--- init-loader/50_magit.el
+++ init-loader/50_magit.el
@@ -1,13 +1,17 @@
 ;; Magit
-(setq-default magit-auto-revert-mode nil)
-(setq vc-handled-backends '())
-(eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
-(define-key global-map (kbd "C-x m") 'magit-status)
-
-(custom-set-faces
- '(magit-diff-added ((t (:background "black" :foreground "green"))))
- '(magit-diff-added-highlight ((t (:background "white" :foreground "green"))))
- '(magit-diff-removed ((t (:background "black" :foreground "blue"))))
- '(magit-diff-removed-hightlight ((t (:background "white" :foreground "blue"))))
- '(magit-hash ((t (:foreground "red"))))
-)
+(use-package magit
+  :bind (
+         ("C-x m" . magit-status)
+         )
+  :config
+  (setq-default magit-auto-revert-mode nil)
+  (setq vc-handled-backends '())
+  (eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
+  (custom-set-faces
+   '(magit-diff-added ((t (:background "black" :foreground "green"))))
+   '(magit-diff-added-highlight ((t (:background "white" :foreground "green"))))
+   '(magit-diff-removed ((t (:background "black" :foreground "blue"))))
+   '(magit-diff-removed-hightlight ((t (:background "white" :foreground "blue"))))
+   '(magit-hash ((t (:foreground "red"))))
+   )
+  )
