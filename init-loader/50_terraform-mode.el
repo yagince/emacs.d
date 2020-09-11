d13f473962001f69ee8c097a3e42e5cf7f510960
yagince <straitwalk@gmail.com>
2020-09-11 23:57:48 +0900

    leaf.elに移行中 terraform-mode

--- init-loader/50_terraform-mode.el
+++ init-loader/50_terraform-mode.el
@@ -1,16 +1,28 @@
-(use-package terraform-mode
-  :defer t
-  :mode (
-         ("\\.tf\\'" . terraform-mode)
-         ("\\.hcl\\'" . terraform-mode)
-         )
+;; (use-package terraform-mode
+;;   :defer t
+;;   :mode (
+;;          ("\\.tf\\'" . terraform-mode)
+;;          ("\\.hcl\\'" . terraform-mode)
+;;          )
+;;   :config
+;;   (add-hook 'terraform-mode-hook
+;;             '(lambda ()
+;;                (company-mode t)
+;;                (dumb-jump-mode t)
+;;                (yas-minor-mode t)
+;;                (setq tab-width 2)
+;;                (terraform-format-on-save-mode t)
+;;                ))
+;;   )
+
+(leaf terraform-mode
+  :mode ("\\.tf\\'" "\\.hcl\\'")
   :config
-  (add-hook 'terraform-mode-hook
-            '(lambda ()
-               (company-mode t)
-               (dumb-jump-mode t)
-               (yas-minor-mode t)
-               (setq tab-width 2)
-               (terraform-format-on-save-mode t)
-               ))
-  )
+  (with-eval-after-load 'terraform-mode
+    (add-hook 'terraform-mode-hook
+              '(lambda nil
+                 (company-mode t)
+                 (dumb-jump-mode t)
+                 (yas-minor-mode t)
+                 (setq tab-width 2)
+                 (terraform-format-on-save-mode t)))))
