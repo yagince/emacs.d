0654721da3cbb1f7a5de000aab3361ca86f014f6
yagince <straitwalk@gmail.com>
2020-09-12 02:29:32 +0900

    init.el以外にcustom-set-*があるとinit.elにカスタムされた値が書き戻されるので消した

--- init-loader/50_magit.el
+++ init-loader/50_magit.el
@@ -24,20 +24,4 @@
     (setq vc-handled-backends 'nil)
     (eval-after-load "vc"
       '(remove-hook 'find-file-hooks 'vc-find-file-hook))
-
-    (custom-set-faces
-     '(magit-diff-added
-       ((t
-         (:background "black" :foreground "green"))))
-     '(magit-diff-added-highlight
-       ((t
-         (:background "white" :foreground "green"))))
-     '(magit-diff-removed
-       ((t
-         (:background "black" :foreground "blue"))))
-     '(magit-diff-removed-hightlight
-       ((t
-         (:background "white" :foreground "blue"))))
-     '(magit-hash
-       ((t
-         (:foreground "red")))))))
+    ))
