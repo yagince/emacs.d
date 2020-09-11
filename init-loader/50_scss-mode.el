f9a4d6042498d743518f45531db9d2a8818b92f6
yagince <straitwalk@gmail.com>
2020-09-11 23:54:38 +0900

    leaf.elに移行中 scss

--- init-loader/50_scss-mode.el
+++ init-loader/50_scss-mode.el
@@ -1,14 +1,31 @@
-(use-package scss-mode
-  :mode (("\\.scss$" . scss-mode))
-  :init
-  (defun scss-custom ()
+;; (use-package scss-mode
+;;   :mode (("\\.scss$" . scss-mode))
+;;   :init
+;;   (defun scss-custom ()
+;;     "scss-mode-hook"
+;;     (and
+;;      (set (make-local-variable 'css-indent-offset) 2) ;; インデント幅を2にする
+;;      (set (make-local-variable 'scss-compile-at-save) nil) ;; コンパイルは compass watchで行うので自動コンパイルをオフ
+;;      )
+;;     )
+;;   :config
+;;   (add-hook 'scss-mode-hook
+;;             '(lambda() (scss-custom)))
+;;   )
+(leaf scss-mode
+  :ensure t
+  :preface
+  (defun scss-custom nil
     "scss-mode-hook"
     (and
-     (set (make-local-variable 'css-indent-offset) 2) ;; インデント幅を2にする
-     (set (make-local-variable 'scss-compile-at-save) nil) ;; コンパイルは compass watchで行うので自動コンパイルをオフ
+     (set (make-local-variable 'css-indent-offset) 2)
+     (set (make-local-variable 'scss-compile-at-save) nil)
      )
     )
+
+  :mode ("\\.scss$")
   :config
-  (add-hook 'scss-mode-hook
-            '(lambda() (scss-custom)))
-  )
+  (with-eval-after-load 'scss-mode
+    (add-hook 'scss-mode-hook
+              '(lambda nil
+                 (scss-custom)))))
