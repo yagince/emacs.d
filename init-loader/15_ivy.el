20e613e41e4b622cca98a4a7ae2fb04ed5db0542
yagince <straitwalk@gmail.com>
2020-09-13 22:07:35 +0900

    add prescient

--- init-loader/15_ivy.el
+++ init-loader/15_ivy.el
@@ -1,4 +1,5 @@
 (leaf ivy
+  :ensure t
   :config
   (leaf all-the-icons-ivy-rich :ensure t)
   (leaf ivy-rich
@@ -33,7 +34,30 @@
            ("C-c i" . counsel-imenu))
     :config
     (with-eval-after-load 'counsel
-      (counsel-mode 1))))
+      (counsel-mode 1)))
+
+  (leaf prescient
+    :doc "Better sorting and filtering(補完候補の使用履歴が新しいものを上に持ってきてくれる)"
+    :req "emacs-25.1"
+    :tag "extensions" "emacs>=25.1"
+    :url "https://github.com/raxod502/prescient.el"
+    :emacs>= 25.1
+    :ensure t
+    :commands (prescient-persist-mode)
+    :custom `((prescient-aggressive-file-save . t)
+              (prescient-save-file . ,(locate-user-emacs-file "prescient")))
+    :global-minor-mode prescient-persist-mode)
+
+  (leaf ivy-prescient
+    :doc "prescient.el + Ivy"
+    :req "emacs-25.1" "prescient-4.0" "ivy-0.11.0"
+    :tag "extensions" "emacs>=25.1"
+    :url "https://github.com/raxod502/prescient.el"
+    :emacs>= 25.1
+    :ensure t
+    :after prescient ivy
+    :custom ((ivy-prescient-retain-classic-highlighting . t))
+    :global-minor-mode t))
 ;; (use-package ivy-rich
 ;;   :ensure t
 ;;   :bind (
--- init-loader/50_magit.el
+++ init-loader/50_magit.el
@@ -1,20 +1,3 @@
-;; Magit
-;; (use-package magit
-;;   :bind (
-;;          ("C-x m" . magit-status)
-;;          )
-;;   :config
-;;   (setq-default magit-auto-revert-mode nil)
-;;   (setq vc-handled-backends '())
-;;   (eval-after-load "vc" '(remove-hook 'find-file-hooks 'vc-find-file-hook))
-;;   (custom-set-faces
-;;    '(magit-diff-added ((t (:background "black" :foreground "green"))))
-;;    '(magit-diff-added-highlight ((t (:background "white" :foreground "green"))))
-;;    '(magit-diff-removed ((t (:background "black" :foreground "blue"))))
-;;    '(magit-diff-removed-hightlight ((t (:background "white" :foreground "blue"))))
-;;    '(magit-hash ((t (:foreground "red"))))
-;;    )
-;;   )
 (leaf magit
   :ensure t
   :bind (("C-x m" . magit-status))
