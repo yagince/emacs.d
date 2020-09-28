017dcb391911cb6b951ac810e347327865cc3fa0
yagince <straitwalk@gmail.com>
2020-09-28 23:08:27 +0900

    add some elisp

--- init-loader/50_other-mode.el
+++ init-loader/50_other-mode.el
@@ -18,3 +18,33 @@
 
 (leaf yaml-mode
   :ensure t)
+
+(leaf highlight-indent-guides
+  :ensure t
+  :hook
+  (yaml-mode . highlight-indent-guides-mode)
+  :custom
+  (highlight-indent-guides-auto-enabled . t)
+  (highlight-indent-guides-responsive . t)
+  (highlight-indent-guides-method . 'character)
+  )
+
+(leaf volatile-highlights
+  :ensure t
+  :config
+  (volatile-highlights-mode)
+  (with-no-warnings
+    (when (fboundp 'pulse-momentary-highlight-region)
+      (defun my-vhl-pulse (beg end &optional _buf face)
+        "Pulse the changes."
+        (pulse-momentary-highlight-region beg end face))
+      (advice-add #'vhl/.make-hl :override #'my-vhl-pulse)))
+  )
+
+(leaf git-gutter+
+  :if (>= emacs-major-version 25)
+  :ensure t
+  :blackout `((git-gutter+-mode
+               . ,(format "%s" (all-the-icons-octicon "git-merge"))))
+  :bind ("C-x G" . global-git-gutter+-mode)
+  )
