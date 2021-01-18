7e0cb9e02c570567ef277cb87f68a8d7908a3e29
yagince <straitwalk@gmail.com>
2021-01-18 14:38:11 +0900

    dumb-jump

--- init-loader/05_dumb_jump.el
+++ init-loader/05_dumb_jump.el
@@ -1,16 +1,3 @@
-;; (use-package dumb-jump
-;;   :defer t
-;;   :bind (
-;;          ("C-M-g" . dumb-jump-go)
-;;          ("C-M-b" . dumb-jump-back)
-;;          ("C-M-q" . dumb-jump-quick-look)
-;;          )
-;;   :config
-;;   (setq dumb-jump-selector 'ivy)
-;;   (bind-keys :map dumb-jump-mode-map
-;;              ("C-M-p" . nil)
-;;              )
-;;   )
 (leaf dumb-jump
   :ensure t
   :bind (("C-M-g" . dumb-jump-go)
@@ -20,7 +7,10 @@
   (with-eval-after-load 'dumb-jump
     (setq dumb-jump-selector 'ivy)
     (bind-keys :map dumb-jump-mode-map
-               ("C-M-p" . nil))))
+               ("C-M-p" . nil)))
+  :hook
+  (xref-backend-functions . dumb-jump-xref-activate)
+  )
 
 (leaf smart-jump
   :ensure t
