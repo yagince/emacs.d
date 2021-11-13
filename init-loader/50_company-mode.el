eec3014247e86ab74067c137d2cda5ccdbf26094
yagince <straitwalk@gmail.com>
2021-11-13 20:55:07 +0900

    rustic-modeでlsp-uiが若干うざいので一旦おふ

--- init-loader/50_company-mode.el
+++ init-loader/50_company-mode.el
@@ -25,24 +25,3 @@
     :custom ((company-box-icons-alist quote company-box-icons-all-the-icons)
              (company-box-doc-enable)))
   )
-
-
-;; (use-package company
-;;   :defer t
-;;   :config
-;;   (bind-keys :map company-active-map
-;;              ("C-n" . company-select-next)
-;;              ("C-p" . company-select-previous)
-;;              ("C-n" . company-select-next)
-;;              ("C-p" . company-select-previous)
-;;              ("C-s" . company-filter-candidates)
-;;              ("M-/" . company-complete-selection)
-;;              )
-;;   )
-
-;; (use-package company-box
-;;   :after (company all-the-icons)
-;;   :hook ((company-mode . company-box-mode))
-;;   :custom
-;;   (company-box-icons-alist 'company-box-icons-all-the-icons)
-;;   (company-box-doc-enable nil))
--- init-loader/50_lsp-mode.el
+++ init-loader/50_lsp-mode.el
@@ -5,28 +5,28 @@
   ;;  (lsp-eslint-auto-fix-on-save . t)
   ;;  )
   :config
-  (leaf lsp-ui
-    :ensure t
-    :custom
-    (
-     ;; (lsp-ui-doc-enable            . nil)
-     (lsp-ui-doc-header            . t)
-     (lsp-ui-doc-include-signature . t)
-     (lsp-ui-doc-position          . 'at-point)
-     (lsp-ui-doc-max-width         . 150)
-     (lsp-ui-doc-max-height        . 30)
-     (lsp-ui-doc-use-childframe    . nil)
-     (lsp-ui-doc-use-webkit        . t)
-     ;; (lsp-ui-flycheck-enable       . nil)
-     (lsp-ui-peek-enable           . t)
-     (lsp-ui-peek-peek-height      . 20)
-     (lsp-ui-peek-list-width       . 50)
-     (lsp-ui-peek-fontify          . 'on-demand) ;; never, on-demand, or always
-     (lsp-ui-imenu-enable          . nil)
-     (lsp-ui-imenu-kind-position   . 'top)
-     (lsp-ui-sideline-enable       . nil)
-     )
-    )
+  ;; (leaf lsp-ui
+  ;;   :ensure t
+  ;;   :custom
+  ;;   (
+  ;;    ;; (lsp-ui-doc-enable            . nil)
+  ;;    (lsp-ui-doc-header            . nil)
+  ;;    (lsp-ui-doc-include-signature . nil)
+  ;;    (lsp-ui-doc-position          . 'at-point)
+  ;;    (lsp-ui-doc-max-width         . 150)
+  ;;    (lsp-ui-doc-max-height        . 30)
+  ;;    (lsp-ui-doc-use-childframe    . nil)
+  ;;    (lsp-ui-doc-use-webkit        . t)
+  ;;    ;; (lsp-ui-flycheck-enable       . nil)
+  ;;    (lsp-ui-peek-enable           . nil)
+  ;;    (lsp-ui-peek-peek-height      . 20)
+  ;;    (lsp-ui-peek-list-width       . 50)
+  ;;    (lsp-ui-peek-fontify          . 'on-demand) ;; never, on-demand, or always
+  ;;    (lsp-ui-imenu-enable          . nil)
+  ;;    (lsp-ui-imenu-kind-position   . 'top)
+  ;;    (lsp-ui-sideline-enable       . nil)
+  ;;    )
+  ;;   )
   :hook
   (web-mode-hook . lsp-deferred)
   ;; (ruby-mode-hook . lsp-deferred)
