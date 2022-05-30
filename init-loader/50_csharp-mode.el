d31f6663e2a5d5f4be19e7ffef7dc43a53da1f97
yagince <straitwalk@gmail.com>
2022-05-30 21:44:29 +0900

    csharp

--- init-loader/50_csharp-mode.el
+++ init-loader/50_csharp-mode.el
@@ -1,11 +1,11 @@
-;; (use-package csharp-mode
-;;   :defer t
-;;   :mode (
-;;          ("\\.cs$'" . csharp-mode)
-;;          ("\\.csx$" . csharp-mode)
-;;          )
-;;   :config
-;;   )
-(leaf csharp-mode
-  :ensure t
-  :mode ("\\.cs$'" "\\.csx$"))
+;; ;; (use-package csharp-mode
+;; ;;   :defer t
+;; ;;   :mode (
+;; ;;          ("\\.cs$'" . csharp-mode)
+;; ;;          ("\\.csx$" . csharp-mode)
+;; ;;          )
+;; ;;   :config
+;; ;;   )
+;; (leaf csharp-mode
+;;   :ensure t
+;;   :mode ("\\.cs$'" "\\.csx$"))
