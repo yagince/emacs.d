4a215f063fd014d53bbd7d19b02606420768c383
yagince <straitwalk@gmail.com>
2022-05-30 21:45:35 +0900

    elixir

--- init.el
+++ init.el
@@ -677,6 +677,31 @@
   :mode ("\\.cs$'" "\\.csx$"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 50_elixir-mode.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(leaf elixir-mode
+  :ensure t
+  :mode ("\\.elixir2\\'" "\\.ex$" "\\.exs$")
+  :config
+  (yas-reload-all)
+  (add-hook 'elixir-mode-hook
+            '(lambda nil
+               (company-mode t)
+               (dumb-jump-mode t)
+               (yas-minor-mode t)))
+  (with-eval-after-load 'elixir-mode
+    (add-to-list 'elixir-mode-hook
+                 (defun auto-activate-ruby-end-mode-for-elixir-mode nil
+                   (set
+                    (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
+                    "\\(?:^\\|\\s-+\\)\\(?:do\\)")
+                   (set
+                    (make-variable-buffer-local 'ruby-end-check-statement-modifiers)
+                    nil)
+                   (ruby-end-mode 1)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 50_web-mode.el
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
