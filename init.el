12d6050902ff7dba55860b05046039a8e94b22be
yagince <straitwalk@gmail.com>
2022-05-30 21:28:43 +0900

    web-modeも移行

--- init.el
+++ init.el
@@ -410,6 +410,66 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 50_web-mode.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(leaf web-mode
+  :ensure t
+  :mode ("\\.phtml$"
+         "\\.tpl\\.php$"
+         "\\.jsp$"
+         "\\.as[cp]x$"
+         "\\.erb$"
+         "\\.html?$"
+         "\\.css?$"
+         "\\.jsx$"
+         "\\.js$"
+         "\\.tsx$"
+         "\\.ts$"
+         )
+  :config
+  (add-hook 'web-mode-hook
+            '(lambda nil
+               (company-mode t)
+               (dumb-jump-mode t)
+               (yas-minor-mode t)))
+
+  (with-eval-after-load 'web-mode
+    (setq auto-save-default nil)
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2)
+    (setq indent-tabs-mode nil)
+    (setq web-mode-enable-auto-pairing t)
+    (setq web-mode-enable-auto-closing t)
+    )
+
+  (leaf nvm
+    :ensure t
+    :config
+    (nvm-use "16.13.0")
+    )
+
+  (leaf add-node-modules-path
+    :ensure t
+    :after nvm
+    :hook
+    (
+     (web-mode-hook . add-node-modules-path)
+     )
+    )
+
+  (leaf prettier
+    :ensure t
+    :after nvm add-node-modules-path
+    :hook
+    (
+     (web-mode-hook . prettier-mode)
+     )
+    )
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (leaf init-loader
   :ensure t
