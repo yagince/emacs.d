ac4d6f4a3543ddec22d5371cfddb5b0ae07543b8
Natsuki Yagi <straitwalk@gmail.com>
2024-04-09 00:25:17 +0900

    remove unused codes

--- init.el
+++ init.el
@@ -1196,27 +1196,6 @@
                                 )
                               )))
 
-  ;; (with-eval-after-load "lsp-rust"
-  ;;   (lsp-register-client
-  ;;    (make-lsp-client
-  ;;     :new-connection (lsp-stdio-connection
-  ;;                      (lambda ()
-  ;;                        `(,(or (executable-find
-  ;;                                (cl-first lsp-rust-analyzer-server-command))
-  ;;                               (lsp-package-path 'rust-analyzer)
-  ;;                               "rust-analyzer")
-  ;;                          ,@(cl-rest lsp-rust-analyzer-server-args))))
-  ;;     :remote? t
-  ;;     :major-modes '(rust-mode rustic-mode)
-  ;;     :initialization-options 'lsp-rust-analyzer--make-init-options
-  ;;     :notification-handlers (ht<-alist lsp-rust-notification-handlers)
-  ;;     :action-handlers (ht ("rust-analyzer.runSingle" #'lsp-rust--analyzer-run-single))
-  ;;     :library-folders-fn (lambda (_workspace) lsp-rust-library-directories)
-  ;;     :after-open-fn (lambda ()
-  ;;                      (when lsp-rust-analyzer-server-display-inlay-hints
-  ;;                        (lsp-rust-analyzer-inlay-hints-mode)))
-  ;;     :ignore-messages nil
-  ;;     :server-id 'rust-analyzer-remote)))
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
